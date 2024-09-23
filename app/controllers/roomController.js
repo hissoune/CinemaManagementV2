1d468b9d2ff080d53e160dc04c49b38a043c5b4e
-m <-mkhalidhissoune962@gmail.com>
2024-09-23 03:45:15 +0100

    add the roomservice

--- app/controllers/roomController.js
+++ app/controllers/roomController.js
@@ -1,106 +1,56 @@
-const Room = require('../models/Room');
-const User = require('../models/User');
+const roomService = require('../services/roomService');
 
 exports.createRoom = async (req, res) => {
   try {
     const userId = req.user.id;
-
-    const userExists = await User.findById(userId);
-    if (!userExists) {
-      return res.status(404).json({ msg: 'User not found' });
-    }
     const { name, capacity, location } = req.body;
-
-    const newRoom = new Room({
-      name,
-      capacity,
-      location,
-      creator:userId,
-    });
-
-    await newRoom.save();
+    const newRoom = await roomService.createRoom(userId, name, capacity, location);
     res.status(201).json(newRoom);
   } catch (err) {
-    console.error(err);
-    res.status(500).json({ msg: 'Server error' });
+    res.status(500).json({ msg: err.message });
   }
 };
 
-// Get all rooms
 exports.getAllRooms = async (req, res) => {
   try {
-     const userId = req.user.id;
-
-    const userExists = await User.findById(userId);
-    if (!userExists) {
-      return res.status(404).json({ msg: 'User not found' });
-    }
-    const rooms = await Room.find({creator:userId});
+    const userId = req.user.id;
+    const rooms = await roomService.getAllRooms(userId);
     res.status(200).json(rooms);
   } catch (err) {
-    console.error(err);
-    res.status(500).json({ msg: 'Server error' });
+    res.status(500).json({ msg: err.message });
   }
 };
 
-// Get a single room by ID
 exports.getRoomById = async (req, res) => {
   try {
-     const userId = req.user.id;
-
-    const userExists = await User.findById(userId);
-    if (!userExists) {
-      return res.status(404).json({ msg: 'User not found' });
-    }
-    const room = await Room.findOne({ _id: req.params.id, creator: userId });
-    if (!room) {
-      return res.status(404).json({ msg: 'Room not found' });
-    }
+    const userId = req.user.id;
+    const roomId = req.params.id;
+    const room = await roomService.getRoomById(roomId, userId);
     res.status(200).json(room);
   } catch (err) {
-    console.error(err);
-    res.status(500).json({ msg: 'Server error' });
+    res.status(500).json({ msg: err.message });
   }
 };
 
-// Update a room
 exports.updateRoom = async (req, res) => {
   try {
-    const userId = req.user.id; 
-
-    const room = await Room.findOne({ _id: req.params.id, creator: userId });
-
-    if (!room) {
-      return res.status(404).json({ msg: 'Room not found or you are not the creator' });
-    }
-
-    const updatedRoom = await Room.findByIdAndUpdate(req.params.id, req.body, { new: true });
-
+    const userId = req.user.id;
+    const roomId = req.params.id;
+    const updateData = req.body;
+    const updatedRoom = await roomService.updateRoom(roomId, userId, updateData);
     res.status(200).json(updatedRoom);
   } catch (err) {
-    console.error(err);
-    res.status(500).json({ msg: 'Server error' });
+    res.status(500).json({ msg: err.message });
   }
 };
 
-
-// Delete a room
 exports.deleteRoom = async (req, res) => {
   try {
-    const userId = req.user.id; 
-
-    const room = await Room.findOne({ _id: req.params.id, creator: userId });
-
-    if (!room) {
-      return res.status(404).json({ msg: 'Room not found or you are not the creator' });
-    }
-
-    await Room.findByIdAndDelete(req.params.id);
-    
-    res.status(200).json({ msg: 'Room deleted successfully' });
+    const userId = req.user.id;
+    const roomId = req.params.id;
+    const result = await roomService.deleteRoom(roomId, userId);
+    res.status(200).json(result);
   } catch (err) {
-    console.error(err);
-    res.status(500).json({ msg: 'Server error' });
+    res.status(500).json({ msg: err.message });
   }
 };
-
---
+++ app/services/roomService.js
@@ -0,0 +1,61 @@
+const Room = require('../models/Room');
+const User = require('../models/User');
+
+exports.createRoom = async (userId, name, capacity, location) => {
+  const userExists = await User.findById(userId);
+  if (!userExists) {
+    throw new Error('User not found');
+  }
+
+  const newRoom = new Room({
+    name,
+    capacity,
+    location,
+    creator: userId,
+  });
+
+  return await newRoom.save();
+};
+
+exports.getAllRooms = async (userId) => {
+  const userExists = await User.findById(userId);
+  if (!userExists) {
+    throw new Error('User not found');
+  }
+
+  return await Room.find({ creator: userId });
+};
+
+exports.getRoomById = async (roomId, userId) => {
+  const userExists = await User.findById(userId);
+  if (!userExists) {
+    throw new Error('User not found');
+  }
+
+  const room = await Room.findOne({ _id: roomId, creator: userId });
+  if (!room) {
+    throw new Error('Room not found');
+  }
+
+  return room;
+};
+
+exports.updateRoom = async (roomId, userId, updateData) => {
+  const room = await Room.findOne({ _id: roomId, creator: userId });
+  if (!room) {
+    throw new Error('Room not found or you are not the creator');
+  }
+
+  const updatedRoom = await Room.findByIdAndUpdate(roomId, updateData, { new: true });
+  return updatedRoom;
+};
+
+exports.deleteRoom = async (roomId, userId) => {
+  const room = await Room.findOne({ _id: roomId, creator: userId });
+  if (!room) {
+    throw new Error('Room not found or you are not the creator');
+  }
+
+  await Room.findByIdAndDelete(roomId);
+  return { msg: 'Room deleted successfully' };
+};
