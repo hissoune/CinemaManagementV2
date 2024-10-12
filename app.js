f82d293eaf675063764378ade2d516afa923ff5b
hissoune <khalidhissoune962@gmail.com>
2024-10-12 22:07:23 +0100

    Fix : re add the .env and install the depedncies and reback the cors

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+
 const express = require('express');
-const connectDB = require('./app/config/db');
 require('dotenv').config();
+
+const connectDB = require('./app/config/db');
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./app/utils/swagger-spec');
 const path = require('path');
@@ -12,10 +14,10 @@ app.use(express.urlencoded({ extended: true }));
 
 
 connectDB();
-// app.use(cors({
-//     origin: 'http://localhost:5173',  // Replace this with your frontend's URL
-//     credentials: true,                // If you need to send cookies or other credentials
-// }));
+app.use(cors({
+    origin: 'http://localhost:5173', 
+    credentials: true,
+}));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 const verifyToken = require('./app/midlwares/verifyToken');
