9fbefb1eb962c1d64a1c9bfc21eeb8c8fc5b1258
hissoune <khalidhissoune962@gmail.com>
2024-10-12 21:54:22 +0100

    Add :adding a ne schema of subscription to filter the user by theyre subscription

---
+++ app/models/Subscription.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const SubscriptionSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  type: {
+    type: String,
+    enum: ['subscribed', 'basic'],
+    required: true,
+  },
+  startDate: {
+    type: Date,
+    default: Date.now,
+  },
+  endDate: {
+    type: Date,
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Subscription', SubscriptionSchema);
