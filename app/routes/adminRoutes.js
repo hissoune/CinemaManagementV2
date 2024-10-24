c977a4ddfa8497baac7c5abe8bdf6840d3fa2d68
hissoune <khalidhissoune962@gmail.com>
2024-10-24 19:41:13 +0100

    Fix fix the admine route to upload images

--- app/routes/adminRoutes.js
+++ app/routes/adminRoutes.js
@@ -1,30 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const adminController = require('../controllers/adminController');
-// const User = require('../models/User')
+const upload = require('../midlwares/multerSetup');
 
-router.post('/', adminController.createUser);
+router.post('/',upload.fields([{ name: 'image', maxCount: 1 }]), adminController.createUser);
 router.get('/', adminController.getAllUsers);
-router.put('/update/:id', adminController.getAllUsers);
+router.put('/update/:id',upload.fields([{ name: 'image', maxCount: 1 }]), adminController.getAllUsers);
 router.get('/:id', adminController.getUserById);
 router.delete('/delete/:id', adminController.deleteUser);
 
-// router.get('/profile', verifyToken, async (req, res) => {
-//     try {
-      
-//     const user = await User.findById(req.user.id).select('-password');
-//     if (!user) {
-//       return res.status(404).json({ message: 'User not found' });
-//     }
-
-//     res.status(200).json({
-//       id: user._id,
-//       name: user.name,
-//       email: user.email,
-//       role: user.role,
-//     });
-//   } catch (err) {
-//     res.status(500).json({ message: 'Server error' });
-//   }
-// });
 module.exports = router;
