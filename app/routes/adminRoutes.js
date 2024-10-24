99d793365b5b1e4fb49eacee0acf24708c3a992e
hissoune <khalidhissoune962@gmail.com>
2024-10-24 20:49:56 +0100

    Add : adding the get admins route

--- app/routes/adminRoutes.js
+++ app/routes/adminRoutes.js
@@ -4,7 +4,7 @@ const adminController = require('../controllers/adminController');
 const upload = require('../midlwares/multerSetup');
 
 router.post('/',upload.fields([{ name: 'image', maxCount: 1 }]), adminController.createUser);
-router.get('/', adminController.getAllUsers);
+router.get('/', adminController.getAllAdmins);
 router.put('/update/:id',upload.fields([{ name: 'image', maxCount: 1 }]), adminController.updateUser);
 router.get('/:id', adminController.getUserById);
 router.delete('/banUser/:id', adminController.banUser);
