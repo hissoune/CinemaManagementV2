e11cc776283481b5737edf30bd478f86d730cb6e
hissoune <khalidhissoune962@gmail.com>
2024-10-24 18:52:39 +0100

    Add : export the statistiques router

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ const sessionRoutes = require('./app/routes/sessionRoutes');
 const reservationRoutes = require('./app/routes/reservationRoutes');
 const publicRoutes = require('./app/routes/publicRoutes')
 const commentsRoutes = require('./app/routes/commentRoute')
+const statiquesRoutes = require('./app/routes/statiqutiquesRoutes');
 
 app.use(verifyToken)
 app.use('/api/auth', authRoutes);
@@ -46,9 +47,8 @@ app.use('/api/rooms', roomRoutes);
 
 app.use('/api/sessions', sessionRoutes);
 
-
 app.use('/api/reservations', reservationRoutes);
-
+app.use('/api/statiques', statiquesRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
